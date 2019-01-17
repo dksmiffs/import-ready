aa5944d979ee7d5af45e00639a448ff4928a3eed
Dave Smith <dave.k.smith@gmail.com>
2019-01-16 21:47:32 -0600

    Rename files for better package flow

--- importable/test_import_module.py
+++
@@ -1,5 +0,0 @@
-import huntsville_havoc_history
-
-print("Havoc founder: " + huntsville_havoc_history.founder())
-print("Established:   " + huntsville_havoc_history.established())
-
--- test_import_package.py
+++
@@ -1,8 +0,0 @@
-from importable import huntsville_havoc_history
-
-print("Havoc founder: " + huntsville_havoc_history.founder())
-
-from importable import established
-
-print("Established:   " + established())
-
