9d9458c11a1424c5d4a9286e59cf94dfa5b7c0ff
Dave Smith <dave.k.smith@gmail.com>
2019-01-17 21:05:51 -0600

    Write initial files needed for TestPyPi upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# importable
+
+This project aims to demonstrate the pieces needed to publish an importable Python package to [TestPyPi].
+
+[TestPyPi]: https://test.pypi.org/
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
+setup(
+   name='importable',
+   version='0.0.1',
+   description='A simple importable Python package',
+   long_description=long_description,
+   long_description_content_type='text/markdown',
+   url='https://gitlab.com/dave.k.smith/importable',
+   author='Dave Smith',
+   author_email='dave.k.smith@gmail.com',
+   classifiers=[
+     'Development Status :: 2 - Pre-Alpha',
+     'Intended Audience :: Developers',
+     'Topic :: Software Development :: Libraries :: Python Modules',
+     'License :: OSI Approved :: MIT License',
+     'License :: OSI Approved :: MIT License',
+   ],
+   keywords='import package huntsville havoc testpypi',
+   packages=find_packages(),
+)
+
